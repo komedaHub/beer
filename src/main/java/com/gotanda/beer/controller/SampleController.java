3d3aac6834a34b2eec1df1d26f120d99d1ebdefb
komedaHub <wearegolden317@gmail.com>
2021-08-08 16:58:37 +0900

    #1 modify endpoint

--- src/main/java/com/gotanda/beer/controller/SampleController.java
+++ src/main/java/com/gotanda/beer/controller/SampleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SampleController {
-    @RequestMapping("/sample2")
+    @RequestMapping("/sample")
     public String sample() {
         return "sample";
     }
